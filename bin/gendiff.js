eac5c52c36ade5fb9fd6c7589f5515ac7ef71343
Andrey Novikov <anovikovrf@gmail.com>
2022-03-05 18:19:07 +0300

    move logic to index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,5 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import startGenDiff from '../src/index.js';
 
-const program = new Command();
-
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>');
-
-program.parse();
+startGenDiff();
