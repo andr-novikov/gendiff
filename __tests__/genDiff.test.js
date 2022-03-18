2c27cd38949cdbf63999da67bc625ea4b5a2f356
Andrey Novikov <anovikovrf@gmail.com>
2022-03-18 19:32:37 +0300

    add plain format tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,21 +1,33 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-import generateDiff from '../src/genDiff.js';
+import genDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('getDifferenceJSON', () => {
-  const diff = generateDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const expectDiff = fs.readFileSync(getFixturePath('expect.txt'), 'utf8');
+test('getDifferenceStylish', () => {
+  const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const expectDiff = fs.readFileSync(getFixturePath('expect-stylish.txt'), 'utf8');
   expect(diff).toEqual(expectDiff);
 });
 
-test('getDifferenceYML', () => {
-  const diff = generateDiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'));
-  const expectDiff = fs.readFileSync(getFixturePath('expect.txt'), 'utf8');
+test('getDifferenceStylishYML', () => {
+  const diff = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'));
+  const expectDiff = fs.readFileSync(getFixturePath('expect-stylish.txt'), 'utf8');
+  expect(diff).toEqual(expectDiff);
+});
+
+test('getDifferencePlain', () => {
+  const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
+  const expectDiff = fs.readFileSync(getFixturePath('expect-plain.txt'), 'utf8');
+  expect(diff).toEqual(expectDiff);
+});
+
+test('getDifferencePlainYML', () => {
+  const diff = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'), 'plain');
+  const expectDiff = fs.readFileSync(getFixturePath('expect-plain.txt'), 'utf8');
   expect(diff).toEqual(expectDiff);
 });
