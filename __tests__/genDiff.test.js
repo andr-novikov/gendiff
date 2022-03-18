d50520b388b4e4f060ba06bcb59e59c560cb2745
Andrey Novikov <anovikovrf@gmail.com>
2022-03-18 15:38:44 +0300

    update fixtures and tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
+import fs from 'fs';
 import generateDiff from '../src/genDiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,19 +8,14 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const str = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
-
-test('getDifferencePLainJSON', () => {
-  expect(generateDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(str);
+test('getDifferenceJSON', () => {
+  const diff = generateDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const expectDiff = fs.readFileSync(getFixturePath('expect.txt'), 'utf8');
+  expect(diff).toEqual(expectDiff);
 });
 
-test('getDifferencePlaintYML', () => {
-  expect(generateDiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'))).toEqual(str);
+test('getDifferenceYML', () => {
+  const diff = generateDiff(getFixturePath('file1.yml'), getFixturePath('file2.yaml'));
+  const expectDiff = fs.readFileSync(getFixturePath('expect.txt'), 'utf8');
+  expect(diff).toEqual(expectDiff);
 });
