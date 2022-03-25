1b6773fdc0ede2528e5a813dafe93989211ed643
Andrey Novikov <anovikovrf@gmail.com>
2022-03-25 17:18:44 +0300

    add formatted output options

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,11 +1,12 @@
 import getDifference from './getDifference.js';
 import getFileContent from './getFileContent.js';
+import formatter from '../formatters/index.js';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = getFileContent(filepath1);
   const data2 = getFileContent(filepath2);
   const diff = getDifference(data1, data2);
-  return diff;
+  return formatter(diff, formatName);
 };
 
 export default genDiff;
--- src/index.js
+++ src/index.js
@@ -8,12 +8,11 @@ export default () => {
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
-    .option('-f, --format <type>', 'output format')
+    .option('-f, --format <type>', 'output format', 'stylish')
     .arguments('<filepath1> <filepath2>')
     .action((filepath1, filepath2) => {
-      const diff = genDiff(filepath1, filepath2);
-      console.log(diff);
+      const options = program.opts();
+      console.log(genDiff(filepath1, filepath2, options.format));
     })
     .parse();
 };
-// const options = program.opts();
