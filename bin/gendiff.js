a7f5ee189a22249804a096468f157f47e5ce6327
Andrey Novikov <anovikovrf@gmail.com>
2022-03-30 22:34:16 +0300

    move from src/index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,18 @@
 #!/usr/bin/env node
+import { Command } from 'commander';
+import genDiff from '../src/genDiff.js';
 
-import generateDiff from '../src/index.js';
+const program = new Command();
 
-generateDiff();
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const { format } = program.opts();
+    const diff = genDiff(filepath1, filepath2, format);
+    console.log(diff);
+  })
+  .parse();
