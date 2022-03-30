3d03905e4057745a690b284ef2bd214ed2674b90
Andrey Novikov <anovikovrf@gmail.com>
2022-03-30 22:36:35 +0300

    refactor

--- formatters/index.js
+++ formatters/index.js
@@ -3,11 +3,12 @@ import plain from './plain.js';
 import json from './json.js';
 
 export default (obj, formatName) => {
-  if (formatName === 'plain') {
-    return plain(obj);
+  switch (formatName) {
+    case 'plain':
+      return plain(obj);
+    case 'json':
+      return json(obj);
+    default:
+      return stylish(obj);
   }
-  if (formatName === 'json') {
-    return json(obj);
-  }
-  return stylish(obj);
 };
--- formatters/plain.js
+++ formatters/plain.js
@@ -1,32 +1,36 @@
 import _ from 'lodash';
 
-const format = (value) => {
-  if (value === null || _.isBoolean(value) || _.isNumber(value)) {
-    return value;
-  } if (_.isObject(value)) {
+const stringify = (value) => {
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+  if (_.isObject(value)) {
     return '[complex value]';
   }
-  return `'${value}'`;
+  return value;
 };
 
 const plain = (value) => {
-  const iter = (currentValue, key) => {
-    const currentKey = key ? `${key}.` : '';
-    const lines = currentValue.map((obj) => {
-      switch (obj.status) {
-        case 'nested':
-          return iter(obj.children, `${currentKey}${obj.key}`);
-        case 'removed':
-          return `Property '${currentKey}${obj.key}' was removed`;
-        case 'added':
-          return `Property '${currentKey}${obj.key}' was added with value: ${format(obj.value)}`;
-        case 'updated':
-          return `Property '${currentKey}${obj.key}' was updated. From ${format(obj.oldValue)} to ${format(obj.newValue)}`;
-        default:
-          return '';
-      }
-    });
-    return _.compact(lines).join('\n');
+  const iter = (currentValue, parent = '') => {
+    const lines = currentValue
+      .filter((obj) => obj.status !== 'unchanged')
+      .map((obj) => {
+        const property = parent ? `${parent}.${obj.key}` : obj.key;
+        switch (obj.status) {
+          case 'nested':
+            return iter(obj.children, property);
+          case 'removed':
+            return `Property '${property}' was removed`;
+          case 'added':
+            return `Property '${property}' was added with value: ${stringify(obj.value)}`;
+          case 'updated':
+            return `Property '${property}' was updated. From ${stringify(obj.oldValue)} to ${stringify(obj.newValue)}`;
+          default:
+            throw new Error(`Unknown difference: '${obj.status}'!`);
+        }
+      });
+
+    return lines.join('\n');
   };
 
   return iter(value);
